3b0ea675eca51fcb8e96d6c9b316da8842ac6ef3
Nemo <assassinste@gmail.com>
2024-09-22 08:21:23 +0800

    feat: External API for inserting and selecting

--- Program.cs
+++ Program.cs
@@ -1,2 +1,100 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using SimpleDatabase.Utility;
+
+namespace SimpleDatabase;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Usage: SimpleDatabase <filename>");
+            return;
+        }
+
+        using (Database db = new Database(args[0]))
+        {
+            Console.WriteLine("Welcome to SimpleDatabase!");
+            Console.WriteLine("Available commands:");
+            Console.WriteLine("[Core]");
+            Console.WriteLine("  select       Print all rows");
+            Console.WriteLine("  insert <id> <username> <email>");
+
+            while (true)
+            {
+                Console.Write("db > ");
+                string input = Console.ReadLine().Trim();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    continue;
+                }
+
+                if (input.StartsWith("."))
+                {
+
+                }
+                else
+                {
+                    string[] parts = input.Split(' ', 4, StringSplitOptions.RemoveEmptyEntries);
+                    if (parts.Length == 0)
+                    {
+                        Console.WriteLine("Empty input");
+                        continue;
+                    }
+
+                    string command = parts[0].ToLower();
+
+                    try
+                    {
+                        switch (command)
+                        {
+                            case "insert":
+                                if (parts.Length != 4)
+                                {
+                                    Console.WriteLine($"Syntax error: insert <id> <username> <email>");
+                                }
+                                else
+                                {
+                                    if (!uint.TryParse(parts[1], out uint id))
+                                    {
+                                        Console.WriteLine("ID must be a non-negative unsigned integer");
+                                    }
+
+                                    string username = parts[2];
+                                    string email = parts[3];
+
+                                    db.Insert(id, username, email);
+                                    Console.WriteLine($"Row inserted ({id}, {username}, {email})");
+                                }
+
+                                break;
+
+                            case "select":
+                                var rows = db.Select();
+
+                                foreach (var row in rows)
+                                {
+                                    Console.WriteLine($"{row.Id}, {row.Username}, {row.Email}");
+                                }
+
+                                Console.WriteLine("Query executed");
+
+                                break;
+
+                            default:
+                                Console.WriteLine($"Unknown command: {command}");
+                                break;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Error: {e.Message}");
+                    }
+                }
+            }
+
+        }
+    }
+}
